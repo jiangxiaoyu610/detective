82ac45c5831b940d872d9b37f2f02b9128ffa84f
bdqsky <bdqsky@163.com>
2019-11-25 20:44:56 +0800

    adjust lr

--- eval.py
+++ eval.py
@@ -13,7 +13,6 @@ from data import SIXray_CLASSES as labelmap
 
 from ssd import build_ssd
 
-import sys
 import os
 import time
 import argparse
@@ -29,7 +28,7 @@ def str2bool(v):
 parser = argparse.ArgumentParser(
     description='Single Shot MultiBox Detector Evaluation')
 parser.add_argument('--trained_model',
-                    default='weights/ssd300_SIXray_11500.pth', type=str,
+                    default='weights/ssd300_SIXray_25000.pth', type=str,
                     help='Trained state_dict file path to open')
 parser.add_argument('--save_folder', default='eval/', type=str,
                     help='File path to save results')
